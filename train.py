451c4c9943a5fccf4da7bcfc80579ebc6a0dd040
Kyle Goyette <kdgoyette@gmail.com>
2021-09-30 10:32:43 -0700

    WIP

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ def main():
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+        datasets.MNIST('./data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])),
